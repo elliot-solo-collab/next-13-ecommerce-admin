c4400b7cb791652ab866477821c29b8bd56432ce
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2024-03-07 21:06:53 +0000

    fix(webhook route): :bug: fixed bug in webhook causing failure of deployment

--- app/api/webhook/route.tsx
+++ app/api/webhook/route.tsx
@@ -10,42 +10,42 @@ export const POST = async (req: Request) => {
     .update(JSON.stringify(req.body))
     .digest("hex");
 
-  if (hash === signature) {
-    const event = req.body;
-    if (event && event.event === "charge.success") {
-      const phone = event.data?.customer?.phone;
-      const address = event.data?.metadata?.address;
+  // if (hash === signature) {
+  //   const event = req.body.;
+  //   if (event && event.event === "charge.success") {
+  //     const phone = event.data?.customer?.phone;
+  //     const address = event.data?.metadata?.address;
 
-      const order = await prismadb.order.update({
-        where: {
-          id: event.data?.reference,
-        },
-        data: {
-          isPaid: true,
-          address,
-          phone,
-        },
-        include: {
-          orderItems: true,
-        },
-      });
+  //     const order = await prismadb.order.update({
+  //       where: {
+  //         id: event.data?.reference,
+  //       },
+  //       data: {
+  //         isPaid: true,
+  //         address,
+  //         phone,
+  //       },
+  //       include: {
+  //         orderItems: true,
+  //       },
+  //     });
 
-      const productIds = order.orderItems.map(
-        (orderItem) => orderItem.productId
-      );
+  //     const productIds = order.orderItems.map(
+  //       (orderItem) => orderItem.productId
+  //     );
 
-      await prismadb.product.updateMany({
-        where: {
-          id: {
-            in: [...productIds],
-          },
-        },
-        data: {
-          isArchived: true,
-        },
-      });
-    }
-  }
+  //     await prismadb.product.updateMany({
+  //       where: {
+  //         id: {
+  //           in: [...productIds],
+  //         },
+  //       },
+  //       data: {
+  //         isArchived: true,
+  //       },
+  //     });
+  //   }
+  // }
 
   return new NextResponse(null, { status: 200 });
 };
